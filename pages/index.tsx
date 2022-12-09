d1325433570d6ca3a256525cc36ac1e803046097
idil <idil.iman1999@gmail.com>
2022-12-09 11:13:48 +0800

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-
+import Header from "../src/components/Header";
 
 export default function Home() {
   return (
@@ -8,7 +8,9 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
       </Head>
-     <h1 className="text-red-500">Test</h1>
+      <h1 className="text-red-500">Test</h1>
+      {/* Header */}
+      <Header />
     </div>
   );
 }
